47c57b1c0f22a83ae8a61146b0809d3fe807639c
Aragorn Nichol <jvmp77@durham.ac.uk>
2025-12-08 17:35:25 +0000

    Update polynomialConvert.m
    Now returns two forms of the Jones polynomial, one in A and one in t

--- polynomialConvert.m
+++ polynomialConvert.m
@@ -1,12 +1,17 @@
 %jonesArray in form [x, a1, a2, a3, ...]
 %x is the highest exponent, the a_n are the coefficients
 % of t^x, t^(x-1), t^(x-2), etc.
-function Jones = polynomialConvert(jonesArray)
-    syms t
+function [Jones_A,Jones_t] = polynomialConvert(jonesArray)
+    syms A t
     highestPower = jonesArray(1);
     jonesArray = jonesArray(2:end);
-    Jones = 0;
+    Jones_A = 0;
+    Jones_t = 0;
     for i = 1:length(jonesArray)
-        Jones = Jones + jonesArray(i)*t^(highestPower-(i-1));
+        %In terms of A
+        Jones_A = Jones_A + jonesArray(i)*A^(highestPower-(i-1));
+        %In terms of t
+        Jones_t = Jones_t + jonesArray(i)*t^((-1/4)*(highestPower-(i-1)));
     end
+    
 end
