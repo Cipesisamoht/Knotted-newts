872709c03ca64bc4c2d2713f81ffb6453e427d01
Breadsticxs <115461617+Breadsticxs@users.noreply.github.com>
2025-12-09 01:23:47 +0000

    Update braids.m
    Comments added and final optimisation tweaks

--- braids.m
+++ braids.m
@@ -1,22 +1,28 @@
-function [PD, arcMat] = braid_to_pd(word, m)
-% Converts a braid word to a planar diagram
+word = [];
+PD_arr = braid_to_pd(word)
 
-    % Ensure inputs are correct
+function [PD_arr] = braid_to_pd(word)
+% Converts a braid word to a planar diagram
+    
+    % Ensure inputs are correct and calculate how many strands are necessary
     word = word(:).';
     n = numel(word);
-    if any(abs(word) < 1 | abs(word) > m-1)
-        error('Each generator index must satisfy 1 <= |k| <= m-1 for m strands.');
+    m = max(abs(word)) + 1;
+    if any(abs(word) < 1
+        error('Invalid braid word inputted');
     end
 
-    % Initialisation
+    % Initialise matrix and starting values
     arcMat = zeros(m, n+1);
     currentArc = 1;
     currentRow = 1;
     arcMat(1, 1) = 1;
 
-    % Fill arcMat
+    % Traverse arcMat with Braid word m times to fill with arc values
     for i = 1:m
         for j = 1:n
+        
+            % Check if the crossings affect current strand path
             k = abs(word(j));
             if currentRow == k
                 currentRow = k + 1;
@@ -27,6 +33,8 @@ function [PD, arcMat] = braid_to_pd(word, m)
             end
             arcMat(currentRow, j+1) = currentArc;
         end
+
+        % Ensure braid connection gives consistent arc values from 1 to n with no gaps
         if arcMat(currentRow, 1) == 0
             arcMat(currentRow, 1) = currentArc;
         else
@@ -37,20 +45,26 @@ function [PD, arcMat] = braid_to_pd(word, m)
     end
 
     % Building Planar Diagram
-    PD = zeros(n, 4);
+    PD_mat = zeros(n, 4);
 
     for j = 1:n
         k = abs(word(j));
 
+        % Find arc values for each crossing
         a = arcMat(k,   j);
         b = arcMat(k+1, j);
         c = arcMat(k+1, j+1);
         d = arcMat(k,   j+1);
 
+        % Define planar diagram notation in matrix form
         if word(j) < 0
-            PD(j, :) = [a, b, c, d];
+            PD_mat(j, :) = [a, b, c, d];
         else
-            PD(j, :) = [b, c, d, a];
+            PD_mat(j, :) = [b, c, d, a];
         end
     end
+
+    % Format as an array for Jones Polynomial function computation
+    PD_arr = reshape(PD_mat.', 1, []);
 end
+
